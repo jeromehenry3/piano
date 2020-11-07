0ae6e4e234e3b1243bacaa99c296befcf95b77d9
jerome <jerome.henry3@gmail.com>
2020-11-07 12:08:30 +0100

    sources change url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,25 +9,25 @@ export class AppComponent {
   audio: any = {};
   constructor() {
     this.audio.c = new Audio();
-    this.audio.c.src = '../assets/C1.mp3';
+    this.audio.c.src = './assets/C1.mp3';
     this.audio.c.load();
     this.audio.d = new Audio();
-    this.audio.d.src = '../assets/D1.mp3';
+    this.audio.d.src = './assets/D1.mp3';
     this.audio.d.load();
     this.audio.e = new Audio();
-    this.audio.e.src = '../assets/E1.mp3';
+    this.audio.e.src = './assets/E1.mp3';
     this.audio.e.load();
     this.audio.f = new Audio();
-    this.audio.f.src = '../assets/F1.mp3';
+    this.audio.f.src = './assets/F1.mp3';
     this.audio.f.load();
     this.audio.g = new Audio();
-    this.audio.g.src = '../assets/G1.mp3';
+    this.audio.g.src = './assets/G1.mp3';
     this.audio.g.load();
     this.audio.a = new Audio();
-    this.audio.a.src = '../assets/A1.mp3';
+    this.audio.a.src = './assets/A1.mp3';
     this.audio.a.load();
     this.audio.b = new Audio();
-    this.audio.b.src = '../assets/B1.mp3';
+    this.audio.b.src = './assets/B1.mp3';
     this.audio.b.load();
   }
 
